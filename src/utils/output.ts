bbb3f1c2db3f881e4b2fe883c20657e25fe8221e
ComBba <cent@naver.com>
2024-06-21 18:07:14 +0900

    chore: update logToOutputChannel function to support custom messages

--- src/utils/output.ts
+++ src/utils/output.ts
@@ -2,6 +2,9 @@ import * as vscode from "vscode";
 
 const outputChannel = vscode.window.createOutputChannel("GPT Commit");
 
-export function logToOutputChannel(message: string) {
+export function logToOutputChannel(message: string, customMessage?: string | undefined) {
+  if (customMessage) {
+    message = message.concat(` ${customMessage}`);
+  }
   outputChannel.appendLine(`GPT Commit: ${message}`);
 }
